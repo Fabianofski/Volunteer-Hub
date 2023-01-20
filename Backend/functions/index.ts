d84422e61356dfb690cfda81a1ab5f8a83c7a418
Fabian Friedrich <friedrich.fabian@gmx.net>
2023-01-20 13:51:35 +0100

    Added profile information dummy Endpoint

--- Backend/functions/index.ts
+++ Backend/functions/index.ts
@@ -1,4 +1,4 @@
-import {Response} from "express";
+import {Response, Request} from "express";
 const functions = require("firebase-functions");
 const express = require("express");
 
@@ -12,6 +12,18 @@ app.get("/api/ping", async (req:Request, res:Response) => {
   res.send("pong");
 });
 
+app.get("/api/profileInformation", async (req:Request<{uid: string}>, res:Response) => {
+  const uid = req.query.uid;
+  res.json({
+    uid: uid,
+    firstname: "Daniel",
+    lastname: "Smith",
+    dateOfBirth: "05.12.2000",
+    email: "daniel.smith@gmail.com",
+    tel: "0176 / 12345656",
+    role: "volunteer",
+  });
+});
 
 app.listen(PORT, () => console.log("Listening ..."));
 exports.app = functions.https.onRequest(app);
