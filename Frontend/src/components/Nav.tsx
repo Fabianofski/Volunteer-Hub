d0bc33bf0f6a134c57d94259370b693552d26c59
Amelia <103031901+Kaninfotola@users.noreply.github.com>
2023-01-24 16:13:40 +0100

    merge fixes whooo

--- Frontend/src/components/Nav.tsx
+++ Frontend/src/components/Nav.tsx
@@ -7,14 +7,13 @@ function Nav({currentUID} : {currentUID: string}) {
     return (
         <div className="nav">
             <a href="/" className="logo">VolunteerHub</a>
+            <div className="nav-right">
             {currentUID === "" ?
                 <a href="/login">Login</a> :
                 <a onClick={() => auth.signOut()} href={"#logout"}>Logout</a>
             }
-            <div className="nav-right">
+            
             <a href="#search">Search</a>
-            <a href="#about">About</a>
-            <a href="/login">Login</a>
             <a href="/about">About</a>
             <a href="/privacy-policy">Privacy Policy</a>
             </div>
