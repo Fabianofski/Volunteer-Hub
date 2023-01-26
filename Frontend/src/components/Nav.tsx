ed5f146ee1b7d28d1a3704080ed6e59914f498d0
Amelia <103031901+Kaninfotola@users.noreply.github.com>
2023-01-26 12:59:29 +0100

    looks like smth idk

--- Frontend/src/components/Nav.tsx
+++ Frontend/src/components/Nav.tsx
@@ -3,21 +3,21 @@ import React from "react";
 import "../App.css";
 import {auth} from "../firebase";
 
-function Nav({currentUID} : {currentUID: string}) {
+function Nav({ currentUID }: { currentUID: string }) {
     return (
         <div className="nav">
-            <a href="/" className="logo">VolunteerHub</a>
-            <div className="nav-right">
-            {currentUID === "" ?
-                <a href="/login">Login</a> :
-                <a onClick={() => auth.signOut()} href={"#logout"}>Logout</a>
-            }
-            
+            <div className="logo">
+                <a href="/" >VolunteerHub</a>
+             </div>
+            <div></div>
             <a href="#search">Search</a>
             <a href="/about">About</a>
             <a href="/privacy-policy">Privacy Policy</a>
-            </div>
-            </div>
+                {currentUID === "" ?
+                    <a href="/login"> <img src="SignIn.png" alt="SignIn" width="90" height="35" /> </a> :
+                    <a onClick={() => auth.signOut()} href={"#logout"}>Logout</a>
+                    }
+            </div>    
   );
 }
 
