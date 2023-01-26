f2d40f381b17b3c9120233682e830dcfcd43352e
Fabian Friedrich <friedrich.fabian@gmx.net>
2023-01-26 18:45:55 +0100

    Fixed Overflow in Nav & Footer

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import "../NavBar.css";
 
 function Footer() {
   return (
--- Frontend/src/components/Nav.tsx
+++ Frontend/src/components/Nav.tsx
@@ -1,24 +1,29 @@
-
 import React from "react";
 import "../App.css";
 import "../NavBar.css";
-import {auth} from "../firebase";
+import { auth } from "../firebase";
 
 function Nav({ currentUID }: { currentUID: string }) {
-    return (
-        <div className="nav">
-            <div className="logo">
-                <a href="/" >VolunteerHub</a>
-             </div>
-            <div></div>
-            <a href="#search">Search</a>
-            <a href="/about">About</a>
-            <a href="/privacy-policy">Privacy Policy</a>
-                {currentUID === "" ?
-                    <a href="/login"> <img src="SignIn.png" alt="SignIn" width="90" height="35" /> </a> :
-                    <a onClick={() => auth.signOut()} href={"#logout"}>Logout</a>
-                    }
-            </div>    
+  return (
+    <div className="nav">
+      <div className="logo">
+        <a href="/">VolunteerHub</a>
+      </div>
+      <div></div>
+      <a href="#search">Search</a>
+      <a href="/about">About</a>
+      <a href="/privacy-policy">Privacy Policy</a>
+      {currentUID === "" ? (
+        <a href="/login">
+          {" "}
+          <img src="SignIn.png" alt="SignIn" width="90" height="35" />{" "}
+        </a>
+      ) : (
+        <a onClick={() => auth.signOut()} href={"#logout"}>
+          Logout
+        </a>
+      )}
+    </div>
   );
 }
 
