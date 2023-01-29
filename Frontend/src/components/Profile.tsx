20916405175dc95d55ead5db5b9ad81e6a8bf9f4
skochp21 <philippkoch832002@gmail.com>
2023-01-29 22:52:30 +0100

    Profile Page basic layout

--- Frontend/src/components/Profile.tsx
+++ Frontend/src/components/Profile.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import {useParams} from "react-router-dom";
 
 function Profile() {
   const { uid } = useParams();
-  
+  fetch(`/api/profileInformation?uid=${uid}`)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+  interface profileData {
+    uid: string;
+    firstname: string;
+    lastname: string;
+    dateOfBirth: string;
+    email: string;
+  }
+
+  const [profileData, setProfileData] = useState<profileData>;
+  const [email, setEmail] = useState(profileData.email)
+
   return (
-    <h1>Profile {uid}</h1>
-   
+    <div>
+    <h1>Profile ID:{uid}<br></br></h1>
+    <p>Email adress:  profileData.email</p>
+    <p>First name:  </p>
+    <p>Last name: </p>
+    <p>Telephone: </p>
+    <p>Date of Birth: </p>
+    </div>
   );
 }
 
