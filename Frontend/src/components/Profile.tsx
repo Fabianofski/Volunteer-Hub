c7be32fa1aba2e586b2e6dd94a3caa3357efefac
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-01-30 08:25:32 +0100

    Fixed Profile Page

--- Frontend/src/components/Profile.tsx
+++ Frontend/src/components/Profile.tsx
@@ -1,32 +1,37 @@
 import React, { useState } from "react";
 import "../App.css";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+interface profileData {
+  uid: string;
+  firstname: string;
+  lastname: string;
+  dateOfBirth: string;
+  email: string;
+  tel: string;
+}
 
 function Profile() {
   const { uid } = useParams();
+  const [profileData, setProfileData] = useState<profileData>();
   fetch(`/api/profileInformation?uid=${uid}`)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-
-  interface profileData {
-    uid: string;
-    firstname: string;
-    lastname: string;
-    dateOfBirth: string;
-    email: string;
-  }
-
-  const [profileData, setProfileData] = useState<profileData>;
-  const [email, setEmail] = useState(profileData.email)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setProfileData(data);
+    });
 
   return (
     <div>
-    <h1>Profile ID:{uid}<br></br></h1>
-    <p>Email adress:  profileData.email</p>
-    <p>First name:  </p>
-    <p>Last name: </p>
-    <p>Telephone: </p>
-    <p>Date of Birth: </p>
+      <h1>
+        Profile ID:{uid}
+        <br></br>
+      </h1>
+      <p>Email address: {profileData?.email}</p>
+      <p>First name: {profileData?.firstname}</p>
+      <p>Last name: {profileData?.lastname}</p>
+      <p>Telephone: {profileData?.tel}</p>
+      <p>Date of Birth: {profileData?.dateOfBirth}</p>
     </div>
   );
 }
