0ff9d8e99252623cc0723aadc63cec568ca36203
Fabian Friedrich <friedrich.fabian@gmx.net>
2023-02-10 17:04:13 +0100

    Upated EventModel in Backend

---
+++ Backend/functions/model/EventModel.ts
@@ -0,0 +1,25 @@
+export interface EventModel {
+  eventId: string;
+  eventName: string;
+  date: Date;
+  time: string;
+  alias: string;
+  organizer: Organizer;
+  minParticipants: number;
+  maxParticipants: number;
+  location: Location;
+  banner: string;
+  about: string;
+}
+
+export interface Organizer {
+  uid: string;
+  name: string;
+}
+
+export interface Location {
+  street: string;
+  houseNumber: string;
+  postalCode: number;
+  town: string;
+}
