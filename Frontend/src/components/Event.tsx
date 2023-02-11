3b2b22c117744dbdee6c2a28c9bfe2950027aea6
Amelia <103031901+Kaninfotola@users.noreply.github.com>
2023-02-11 20:56:11 +0100

    :pig:

--- Frontend/src/components/Event.tsx
+++ Frontend/src/components/Event.tsx
@@ -1,13 +1,20 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import { auth } from "../firebase";
 import { Route, useParams } from "react-router-dom";
 import "../App.css";
 import "../Event.css";
 import { EventModel } from "../model/EventModel";
+import { errorCodes } from "./Authentication/ErrorCodes";
+
+async function addToDataBase(thisEventId: string | undefined, userId: string | undefined) {
+  //wohoo
+}
 
 function Event() {
   const { eventId } = useParams();
   const [EventModel, setEventData] = useState<EventModel>();
+  const [error, setError] = useState<string>("-");
 
   useEffect(() => {
     fetch(`/api/eventInformation?eventId=${eventId}`)
@@ -17,6 +24,20 @@ function Event() {
         setEventData(data);
       });
   }, [eventId]);
+  function getUserId() {
+    if (auth.currentUser?.uid != undefined) {
+      return auth.currentUser?.uid;
+    }
+  }
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    addToDataBase(EventModel?.eventId, getUserId()).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      setError(errorCodes[errorCode] || errorCode);
+    });
+  };
 
   return (
     <div className="seite">
@@ -28,7 +49,10 @@ function Event() {
           <div className="eventDescriptionLeft">
             <h1>{EventModel?.eventName}</h1>
             <h2>
-              Organizator: <a href="/profile/req.query.uid">{EventModel?.organizer.name}</a>
+              Organizator:{" "}
+              <a href="/profile/" onClick="location.href=this.href+'/'+uid;return false;">
+                {EventModel?.organizer.name}
+              </a>
             </h2>
           </div>
           <div className="eventDescriptionRight">
@@ -46,12 +70,15 @@ function Event() {
         </div>
         <h3>{EventModel?.about}</h3>
         <div className="eventDescription">
-          <h2>Freie Plätze: 2/{EventModel?.maxParticipants}</h2>
+          <h2>
+            Freie Plätze: {EventModel?.currentParticipants}/{EventModel?.maxParticipants}
+          </h2>
         </div>
       </div>
-      <div className="participate">
+      <button className="participate" type="submit" onClick={onSubmit}>
         <h2>Teilnehmen</h2>
-      </div>
+      </button>
+      <p>{auth.currentUser?.uid}</p>
     </div>
   );
 }
